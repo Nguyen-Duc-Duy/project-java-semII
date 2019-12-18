d8ba53904c41ade1ad44cb7999e441af49a61b97
Duy <duy44204@gmail.com>
2019-12-18 09:54:24 +0700

    import project

---
+++ Project/XIXAO Manager/src/Controllers/login.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author NGUYEN DUC DUY
+ */
+public class login {
+    Connection c;
+
+    public login(Connection c) {
+        this.c = c;
+    }
+    
+}
